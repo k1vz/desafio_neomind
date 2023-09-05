40e844eb12e5e86abeb741b99ef57fdcb8e74a12
k1vz <mrnerd777@gmail.com>
2023-09-05 04:15:40 -0300

    changing Card.tsx

--- src/main/frontend/src/components/Card.tsx
+++ src/main/frontend/src/components/Card.tsx
@@ -38,79 +38,75 @@ export const Card = ({ id, name, email, comment, cnpj, refreshData, creating = f
   };
 
   return (
-    <div>
-      <div className="card border-success m-5" style={{ maxWidth: "24rem", minWidth: "24rem" }}>
-        <div className="card-header bg-transparent border-success">
-          CNPJ: {!editing ? <span>{cnpj}</span> : <input
-            value={currentCnpj}
-            onChange={e => setCurrentCnpj(e.target.value)}
-            type="text"
-            className="form-control"
-            placeholder="Enter CNPJ"
-          />}
-        </div>
-        <div className="card-body text-success">
-          <h5 className="card-title">
-            Name: {!editing ? <span>{name}</span> : <input
-              value={currentName}
-              onChange={e => setCurrentName(e.target.value)}
-              type="text"
-              className="form-control"
-              placeholder="Enter name"
-            />}
-          </h5>
-          <p className="card-text">
-            Email: {!editing ? <span>{email}</span> : <input
-              value={currentEmail}
-              onChange={e => setCurrentEmail(e.target.value)}
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-            />}
-          </p>
-          <p className="card-text">
-            Comment: {!editing ? <span>{comment}</span> : <input
-              value={currentComment}
-              onChange={e => setCurrentComment(e.target.value)}
-              type="text"
-              className="form-control"
-              placeholder="Enter comment"
-            />}
-          </p>
-        </div>
-        <div className="card-footer border-top p-2 d-flex justify-content-around">
-          {editing ? (
-            <button
-              type="button"
-              className="btn btn-success p-2"
-              onClick={() => creating ? handleAdd() : handleUpdate()}
-            >
-              Execute
-            </button>
-          ) : null}
-          {!creating ? (
-            <button
-              type="button"
-              className="btn btn-info p-2"
-              data-dismiss="modal"
-              onClick={() => editing ? setEditing(false) : setEditing(true)}
-            >
-              {editing ? "Cancel" : "Update"}
-            </button>
-          ) : null}
-          {!creating ? (
-            <button
-              type="button"
-              className="btn btn-danger p-2"
-              data-dismiss="modal"
-              onClick={handleDelete}
-            >
-              Delete
-            </button>
-          ) : null}
-        </div>
-      </div>
-    </div>
-  );
-};
-
+	<div className="card border-success m-5" style={{ maxWidth: "24rem", minWidth: "24rem" }}>
+	<div className="card-header bg-transparent border-success">
+		CNPJ: {!editing ? <span>{cnpj}</span> : <input
+		value={currentCnpj}
+		onChange={e => setCurrentCnpj(e.target.value)}
+		type="text"
+		className="form-control"
+		placeholder="Enter CNPJ"
+		/>}
+	</div>
+	<div className="card-body text-success">
+		<h5 className="card-title">
+		Name: {!editing ? <span>{name}</span> : <input
+			value={currentName}
+			onChange={e => setCurrentName(e.target.value)}
+			type="text"
+			className="form-control"
+			placeholder="Enter name"
+		/>}
+		</h5>
+		<p className="card-text">
+		Email: {!editing ? <span>{email}</span> : <input
+			value={currentEmail}
+			onChange={e => setCurrentEmail(e.target.value)}
+			type="email"
+			className="form-control"
+			placeholder="Enter email"
+		/>}
+		</p>
+		<p className="card-text">
+		Comment: {!editing ? <span>{comment}</span> : <input
+			value={currentComment}
+			onChange={e => setCurrentComment(e.target.value)}
+			type="text"
+			className="form-control"
+			placeholder="Enter comment"
+		/>}
+		</p>
+	</div>
+	<div className="card-footer border-top p-2 d-flex justify-content-around">
+		{editing ? (
+		<button
+			type="button"
+			className="btn btn-success p-2"
+			onClick={() => creating ? handleAdd() : handleUpdate()}
+		>
+			Execute
+		</button>
+		) : null}
+		{!creating ? (
+		<button
+			type="button"
+			className="btn btn-info p-2"
+			data-dismiss="modal"
+			onClick={() => editing ? setEditing(false) : setEditing(true)}
+		>
+			{editing ? "Cancel" : "Update"}
+		</button>
+		) : null}
+		{!creating ? (
+		<button
+			type="button"
+			className="btn btn-danger p-2"
+			data-dismiss="modal"
+			onClick={handleDelete}
+		>
+			Delete
+		</button>
+		) : null}
+	</div>
+	</div>
+  )}
